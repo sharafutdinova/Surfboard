5f539217794f87f7b425922bf9a18f24ce8c91ca
Шарафутдинова Алсу <sharafutdinova.a@autostels.ru>
2023-08-25 23:34:17 +0300

    fix

--- scripts/menu-acco.js
+++ scripts/menu-acco.js
@@ -1,11 +1,9 @@
 const isTablets = window.matchMedia("(max-width: 768px)").matches;
 const isMobl = window.matchMedia("(max-width: 480px)").matches;
-const screenWidth = $(window).width();
-const container = item.closest(".products-menu");
-const titleBlocks = container.find(".products-menu__title");
 const measureWidth = item => {
     let reqItemWidth = 0;
     const container = item.closest(".products-menu");
+    const screenWidth = $(window).width();
     const titleBlocks = container.find(".products-menu__title");
     const titleWidth = titleBlocks.width() * titleBlocks.length;
     if (isTablets) {
